0bf1336cbee791640116a749c955a02e324cc679
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-19 00:15:30 +0100

    Outline a few functions to implement.

--- R/codecheck.R
+++ R/codecheck.R
@@ -16,3 +16,43 @@ create_codecheck_files <- function() {
   file.copy(file.path(templates, "codecheck.yml"), ".")
   file.copy(file.path(templates, "codecheck"), ".", recursive=TRUE)
 }
+
+##' Return the metadata for the codecheck project in root folder of project
+##'
+##' 
+##' @title Return the metadata for the codecheck project in root folder of project
+##' @param root Path to the root folder of the project.
+##' @return A list containing the metadata found in the codecheck.yml file
+##' @author Stephen Eglen
+codecheck_metadata <- function(root) {
+  read_yaml( file.path(root, "codecheck.yml") )
+}
+
+
+##' Copy manifest files into the root/codecheck/outputs folder; return manifest.
+##'
+##' The metadata should specify the manifest -- the files to copy into the
+##' codecheck/oututs folder.  Each of the files in the manifest is copied into
+##' the destination directory and then the manifest is returned as a dataframe.
+##' @title 
+##' @param root - Path to the root folder of the proejct.
+##' @param metadata - the codecheck metadata list.
+##' @return A dataframe containing one row per manifest file.
+##' @author Stephen Eglen
+.copy_manifest_files <- function(root, metadata) {
+}
+
+######################################################################
+## Code for woking with zenodo records.
+
+.create_zenodo_record <- function(zen) {
+}
+
+.set_zenodo_metadata <- function(zen, metadata) {
+}
+
+.set_zenodo_certificate <- function(zen, certificate="codecheck.pdf") {
+}
+
+## We deliberately do not provide a function to publish the certificate.
+## You should go check it yourself.
