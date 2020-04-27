03d83f92fb346ef65c4a683b9617bc19e8441fa6
Stephen J. Eglen <sje30@cam.ac.uk>
2020-04-27 10:37:50 +0100

    Update codecheck-zenodo.R after working on 2020-009

--- inst/extdata/templates/codecheck-zenodo.R
+++ inst/extdata/templates/codecheck-zenodo.R
@@ -1,7 +1,6 @@
-## Interactive script for Zenodo deposit.
+## Zenodo deposit; see vignette("codecheck_overview.Rmd")
 
 require(codecheck)
-
 metadata = read_yaml( "../codecheck.yml") 
 
 ## To interact with the Zenodo API, you need to create a token.  This should
@@ -13,11 +12,13 @@ my_token = system("pass show codechecker-token", intern=TRUE)
 zenodo <- ZenodoManager$new(token = my_token)
 
 
-## If you wish to create a new record on zenodo, run the following line once
-## and then store the URL of the record in  ../codecheck.yml
+## To create a new Zenodo record, run the following line ONCE
+## and update the report: line of  ../codecheck.yml
 ## This will generate a new record every time that you run it.
+## After creating the record, reload the metadata above.
+##
 ## record = create_zenodo_record(zenodo)
-## you will then to reload the yml
+
 
 record = get_zenodo_record(metadata$report)
 set_zenodo_metadata(zenodo, record, metadata)
@@ -26,5 +27,5 @@ set_zenodo_metadata(zenodo, record, metadata)
 ## delete it via the web page before uploading it again.
 ## set_zenodo_certificate(zenodo, record, "codecheck.pdf") 
 
-## Now go to zenodo and check the record (the URL for the deposit is
-## printed in get_zenodo_record() ) and then publish.
+## Now go to zenodo and check the record (the URL is printed
+## by set_zenodo_metadata() ) and then publish.
