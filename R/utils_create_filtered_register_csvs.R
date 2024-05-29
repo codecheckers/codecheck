d447fdae5cf7ddb9b74a98d43e46a3fea97d88a6
angelina-momin <angelina.momin53@gmail.com>
2024-05-29 14:36:34 +0200

    Create register.csvs for each codechecker

--- R/utils_create_filtered_register_csvs.R
+++ R/utils_create_filtered_register_csvs.R
@@ -8,10 +8,32 @@ create_filtered_register_csvs <- function(filter_by, register){
 
   for (filter in filter_by){
     column_name <- determine_filter_column_name(filter)
-    unique_values <- unique(register[[column_name]])
 
+    if (filter == "codecheckers"){
+      register <- read.csv("docs/temp_register_codechecker.csv", as.is = TRUE)
+      # Once the temp_register is loaded, we can remove it
+      remove("docs/temp_register_codechecker.csv")
+      unique_values <- names(CONFIG$DICT_ORCID_ID_NAME)
+    }
+
+    else{
+      unique_values <- unique(register[[column_name]])
+    }
+
+    # Filtering the register
     for (value in unique_values) {
-      filtered_register <- register[register[[column_name]]==value, ]
+      # In case of filtering by codechecker we need to check if unique value is contained
+      # in the list which is the row value
+      if (column_name == "Codechecker"){
+        mask <- sapply(register$Codechecker, function(x) value %in% fromJSON(x))
+        filtered_register <- register[mask, ]
+      }
+
+      # Else we check against the row value itself
+      else{
+        filtered_register <- register[register[[column_name]]==value, ]
+      }
+    
       output_dir <- paste0(get_output_dir(filter, value), "register.csv")
 
       if (!dir.exists(dirname(output_dir))) {
@@ -30,6 +52,7 @@ create_filtered_register_csvs <- function(filter_by, register){
 determine_filter_column_name <- function(filter) {
   filter_column_name <- switch(filter,
          "venues" = "Type",
+         "codecheckers" = "Codechecker",
          NULL # Default case is set to NULL
          )
   if (is.null(filter_column_name)) {
@@ -60,6 +83,13 @@ get_output_dir <- function(filter, column_value) {
     venue_name <- trimws(gsub("[()]", "", gsub(venue_category, "", column_value)))
     venue_name <- gsub(" ", "_", venue_name)
     return(paste0("docs/", filter, "/", venue_category, "/", venue_name, "/"))  }
+
+  else if (filter=="codecheckers"){
+    # The codechecker column is always a list of codecheckers
+    for (codechecker in column_value){
+      return(paste0("docs/", filter, "/", gsub(" ", "_", codechecker), "/"))
+    }
+  }
 
   else{
     return(paste0("docs/", filter, "/", gsub(" ", "_", tolower(column_value)), "/"))
