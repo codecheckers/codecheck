c84051ce3af21aa719e8f838764e1725f53b51c9
Angelina Momin <angelina.momin53@gmail.com>
2024-09-03 10:19:02 +0200

    Update render_register_json

--- R/utils_render_register_json.R
+++ R/utils_render_register_json.R
@@ -49,37 +49,36 @@ set_paper_title_references <- function(register_table){
 
 #' Renders register json for a single register_table
 #' 
-#' @param filter The filter
 #' @param register_table The register table
-#' @param register_table_name The register table name
-#' @param filter_subcategory An optional string representing a subcategory within the filter 
-#'        (e.g., venue type). Default is NULL.
-render_register_json <- function(filter, register_table, register_table_name, filter_subcategory = NULL) {
+#' @param table_details List containing details such as the table name, subcat name.
+#' @param filter The filter
+render_register_json <- function(register_table, table_details, filter) {
   register_table <- add_repository_links_json(register_table)
 
   # Set paper titles and references
   register_table <- set_paper_title_references(register_table)
 
-  output_dir <- get_output_dir(filter, register_table_name, filter_subcategory)
-  if (!dir.exists(output_dir)) {
-    dir.create(output_dir, recursive = TRUE, showWarnings = TRUE)
-  }
+  output_dir <- table_details[["output_dir"]]
+
+  # Keeping only those columns that are mentioned in the json columns and those that 
+  # register table already has
+  columns_to_keep <- intersect(CONFIG$JSON_COLUMNS, names(register_table))
 
   jsonlite::write_json(
-    register_table[, CONFIG$JSON_COLUMNS],
+    register_table[, columns_to_keep],
     path = paste0(output_dir, "register.json"),
     pretty = TRUE
   )
 
   jsonlite::write_json(
-    utils::tail(register_table, 10)[, CONFIG$JSON_COLUMNS],
+    utils::tail(register_table, 10)[, columns_to_keep],
     path = paste0(output_dir, "featured.json"),
     pretty = TRUE
   )
 
   jsonlite::write_json(
     list(
-      source = generate_href(filter, register_table_name, "json", filter_subcategory),
+      source = generate_href(filter, table_details, "json"),
       cert_count = nrow(register_table)
       # TODO count conferences, preprints,
       # journals, etc.
@@ -88,28 +87,4 @@ render_register_json <- function(filter, register_table, register_table_name, fi
     path = paste0(output_dir, "/stats.json"),
     pretty = TRUE
   )
-}
-
-#' Renders register jsons for a list of register tables
-#' 
-#' @param list_register_table List of register tables
-render_register_jsons <- function(list_register_tables){
-  for (filter in names(list_register_tables)){
-    # For the case of venues we have a nested list
-    if (filter == "venues"){
-      for (venue_subcat in names(list_register_tables[[filter]])){
-        for (venue_name in names(list_register_tables[[filter]][[venue_subcat]])){
-          register_table <- list_register_tables[[filter]][[venue_subcat]][[venue_name]]
-          render_register_json(filter, register_table, venue_name, venue_subcat)
-        }
-      }
-    }
-
-    else{
-      for (register_table_name in names(list_register_tables[[filter]])){
-        register_table <- list_register_tables[[filter]][[register_table_name]]
-        render_register_json(filter, register_table, register_table_name)
-      }
-    }
-  }
 }
