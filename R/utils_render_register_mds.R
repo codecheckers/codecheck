a56f6feadcbc51390bc23f748c6e716d9e04ef8f
Angelina Momin <angelina.momin53@gmail.com>
2024-09-03 10:47:39 +0200

    Update column widths variable name

--- R/utils_render_register_mds.R
+++ R/utils_render_register_mds.R
@@ -127,7 +127,7 @@ create_md_table <- function(register_table, table_details, filter){
   md_table <- unlist(strsplit(md_table, "\n", fixed = TRUE))
   # Determining which line to add the md column widths in
   alignment_line_index <- grep("^\\|:---", md_table)
-  md_table[alignment_line_index] <- CONFIG$MD_COLUMNS_WIDTHS[[filter]]
+  md_table[alignment_line_index] <- CONFIG$MD_TABLE_COLUMN_WIDTHS[[filter]]
 
   return(md_table)
 }
