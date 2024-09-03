a027faaf2e674f1191648f5c8c03323225fb4233
Angelina Momin <angelina.momin53@gmail.com>
2024-09-03 21:03:12 +0200

    Update register grouping methods

--- R/utils_render_register_general.r
+++ R/utils_render_register_general.r
@@ -20,7 +20,6 @@ create_register_files <- function(register_table, filter_by, outputs){
     render_register(register_table, table_details, filter="none", output_type)
   }
 
-  stop()
   # Generating filtered register table files
   # For each filter type we created the nested register tables first
   for (filter in filter_by){
@@ -32,49 +31,55 @@ create_register_files <- function(register_table, filter_by, outputs){
 
     # Group the register_table by the filter column and nest the resulting groups
     filter_col_name <- CONFIG$FILTER_COLUMN_NAMES[[filter]]
-    nested_register_tables <- register_table %>%
-      group_by(across(all_of(filter_col_name))) %>%
-      tidyr::nest()
+    grouped_registers <- register_table %>%
+      group_by(across(all_of(filter_col_name))) 
 
-    # Creating a register.csv file
-    # render_csvs()
+    # Split into a list of data frames
+    filtered_register_list <- grouped_registers %>% group_split()
+
+    # Get the group names (keys) based on the filter names
+    register_keys <- grouped_registers %>% group_keys()
 
     # Looping over each of the output types
     for (output_type in outputs){
-      
-      # Loop over each nested table generated from the filtering
-      for (i in seq_along(nested_register_tables[["data"]])) {
-        
-        nested_table <- nested_register_tables[["data"]][[i]]
-        table_details <- generate_nested_reg_table_details(nested_register_tables, i, filter)
-        
-        # Dropping the subcat column as it will have the same value
-        if (filter %in% names(CONFIG$FILTER_SUBCAT_COLUMNS)){
-          subcat_col <- CONFIG$FILTER_SUBCAT_COLUMNS[[filter]]
-          nested_table <- nested_table[ , !(names(nested_table) %in% subcat_col)]
-        }
 
-        # Render the register file in the specified format
-        render_register(nested_table, table_details, filter, output_type)
+      for (i in seq_along(filtered_register_list)) {
+        # Retrieving the register and its key
+        register_key <- register_keys[i, ]
+        register_table <- filtered_register_list[[i]]
+
+        table_details <- generate_table_details(register_key, register_table, filter)
+
+        # Dropping columns that are redundant
+        register_table <- drop_register_columns(register_table, filter)
+        render_register(register_table, table_details, filter, output_type)
       }
     }
   }
 }
 
-generate_nested_reg_table_details <- function(nested_register_tables, i, filter){
+drop_register_columns <- function(register_table, filter){
+  columns_to_drop <- CONFIG$FILTER_COLUMN_NAMES_TO_DROP[[filter]]
+
+  if (!is.list(columns_to_drop)){columns_to_drop <- list(columns_to_drop)}
+  register_table <- register_table[ , -which(names(register_table) %in% columns_to_drop)]
+  return(register_table)
+}
+
+generate_table_details <- function(table_key, table, filter, is_reg_table = TRUE){
   table_details <- list()
-  filter_col_name <- CONFIG$FILTER_COLUMN_NAMES[[filter]]
 
-  nested_table <- nested_register_tables[["data"]][[i]]
+  # This information is needed when creating the html index section files
+  table_details[["is_reg_table"]] <- is_reg_table
 
-  # The is_reg_table boolean is needed for generating correct index section files
-  table_details[["is_reg_table"]] <- TRUE
-  table_details[["name"]] <- nested_register_tables[[filter_col_name]][[i]]
+  # Setting the table name
+  table_details[["name"]] <- table_key
   table_details[["slug_name"]] <- tolower(gsub(" ", "_", table_details[["name"]]))
-
+  
+  # Checking if the filter has a subcategory
   if (filter %in% names(CONFIG$FILTER_SUBCAT_COLUMNS)){
     subcat_col <- CONFIG$FILTER_SUBCAT_COLUMNS[[filter]]
-    table_details[["subcat"]] <- nested_table[[subcat_col]][1]
+    table_details[["subcat"]] <- table[[subcat_col]][1]
   }
 
   # Generating the output dir once here instead of multiple times
@@ -91,10 +96,10 @@ render_register <- function(register_table, table_details, filter, output_type){
   )
 }
 
-#' Get Output Directory Path
+#' Generate Output Directory Path
 #'
-#' Determines the directory path for saving files based on info in the register table
-#' and the filter name
+#' Generates the directory path for saving files based on info in the register table
+#' and the filter name. Creates the output directory if it does not already exist
 #'
 #' @param filter The filter name (e.g., "venues", "codecheckers").
 #' @param table_details List containing details such as the table name, subcat name.
