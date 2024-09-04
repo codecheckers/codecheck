9fd0c313fbacf84fcfbc4d54ec7e65c9d980b162
Angelina Momin <angelina.momin53@gmail.com>
2024-09-04 19:26:48 +0200

    Remove stop from register_render

--- R/register.R
+++ R/register.R
@@ -35,7 +35,6 @@ register_render <- function(register = read.csv("register.csv", as.is = TRUE),
   create_filtered_reg_csvs(register, filter_by)
   create_register_files(register_table, filter_by, outputs)
   create_non_register_files(register_table, filter_by)
-  stop()
 }
 
 #' Function for checking all entries in the register
