2e0e3119d7ba200fc94fbc21c9ccd39807dee99b
Daniel Nüst <daniel.nuest@tu-dresden.de>
2024-09-11 15:49:18 +0200

    align list entries for readability

--- inst/extdata/config.R
+++ inst/extdata/config.R
@@ -21,8 +21,8 @@ CONFIG$MD_TABLE_COLUMN_WIDTHS <- list(
 # These are the columns to keep in the register table
 CONFIG$REGISTER_COLUMNS <- list(
   other = c("Certificate", "Paper Title", "Type", "Venue", "Issue", "Report", "Check date"),
-  csv = c("Certificate", "Repository", "Type", "Venue", "Issue", "Report", "Check date"),
-  json = c("Certificate", "Repository", "Type", "Venue", "Issue", "Report", "Check date")
+  csv =   c("Certificate", "Repository", "Type", "Venue", "Issue", "Report", "Check date"),
+  json =  c("Certificate", "Repository", "Type", "Venue", "Issue", "Report", "Check date")
 )
 
 CONFIG$DIR_TEMP_REGISTER_CODECHECKER <- "docs/temp_register_codechecker.csv"
