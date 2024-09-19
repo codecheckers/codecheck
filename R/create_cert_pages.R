2abc6826f0c2bfb0238da4bdcbe9eedfd46dda7c
Angelina Momin <angelina.momin53@gmail.com>
2024-09-19 17:49:19 +0200

    Get cert rendering working

--- R/create_cert_pages.R
+++ R/create_cert_pages.R
@@ -1,14 +1,19 @@
 
-render_cert_htmls <- function(register, force_download = FALSE){
+render_cert_htmls <- function(register_table, register, force_download = FALSE){
 
   # Read template
-  html_template <- readLines(CONFIG$CERTS_DIR[[cert_page_template]])
+  html_template <- readLines(CONFIG$CERTS_DIR[["cert_page_template"]])
 
   # Loop over each cert in the register table
-  for (i in 1:nrow(register)){
+  for (i in 1:1){
+  # for (i in 1:nrow(register)){
     # Retrieving report link and cert id
-    report_link <- register[i, ]$Report
-    cert_id <- register[i, ]$Certificate
+    report_link <- register_table[i, ]$Report
+    cert_id <- register_table[i, ]$Certificate
+
+    # if (!(cert_id == "2021-001")){
+    #   next
+    # }
 
     # Define paths for the certificate PDF and JPEG
     pdf_path <- file.path(CONFIG$CERTS_DIR[["cert"]], paste0(cert_id, ".pdf"))
@@ -16,14 +21,19 @@ render_cert_htmls <- function(register, force_download = FALSE){
     
     # Download the PDF if it doesn't exist or if force_download is TRUE
     if (!pdf_exists || force_download) {
-      download_cert_pdf(report_link, cert_id)
-      convert_cert_pdf_to_jpeg(cert_id)
+      # download_cert_status <- download_cert_pdf(report_link, cert_id)
+
+      # # Failed in downloading cert
+      # if (download_cert_status == 0){
+      #   next
+      # }
+      # convert_cert_pdf_to_jpeg(cert_id)
 
       # Delaying reqwuests to adhere to request limits
       Sys.sleep(CONFIG$CERT_REQUEST_DELAY)
     }
 
-    create_cert_md(cert_id)
+    create_cert_md(cert_id, register[i, ]$Repo)
     render_cert_html(cert_id)
   }
 }
@@ -31,24 +41,23 @@ render_cert_htmls <- function(register, force_download = FALSE){
 convert_cert_pdf_to_jpeg <- function(cert_id){
 
   # Checking if the certs dir exist
-  cert_pdf_dir <- CONFIG$CERTS_DIR[["certs_pdf"]]
-  pdf_path <- paste0(cert_id, "cert.pdf") 
+  cert_dir <- paste0(CONFIG$CERTS_DIR[["cert"]], cert_id, "/") 
 
   # Get the number of pages in the PDF
   cert_pdf_path <- paste0(cert_dir, "cert.pdf")
-  num_pages <- pdf_info(cert_pdf_path)$pages
+  num_pages <- pdftools::pdf_info(cert_pdf_path)$pages
 
   # Create image filenames
   image_filenames <- sapply(1:num_pages, function(page) paste0(cert_dir, "cert_", page, ".png"))
   
   # Read and convert PDF to PNG images
-  pdf_convert(cert_pdf_path, format = "png", filenames = image_filenames, dpi = 300)
+  pdftools::pdf_convert(cert_pdf_path, format = "png", filenames = image_filenames, dpi = CONFIG$CERT_DPI)
 }
 
 
 # Creates a markdown file of the certificate which is then rendered
 # to html
-create_cert_md <- function(cert_id){
+create_cert_md <- function(cert_id, repo_link){
 
   # Loading the template
   md_content <- readLines(CONFIG$TEMPLATE_DIR[["cert"]][["md_template"]])
@@ -57,31 +66,37 @@ create_cert_md <- function(cert_id){
   title <- paste(CONFIG$MD_TITLES[["certs"]], cert_id)
   md_content <- gsub("\\$title\\$", title, md_content)
 
-  # Replacing the list of images for the slider
   # Identifying the number of cert pages 
-  no_cert_pages <- length(list.files(path = output_dir, pattern = "^cert_.*\\.jpg$", full.names = TRUE))
-  # Creating a list of images to slide through based on number of cert pages
-  list_images <- paste0('"cert_', 1:no_cert_pages, '.jpg"', collapse = ", ")
+  cert_dir <- paste0(CONFIG$CERTS_DIR[["cert"]], cert_id, "/")
+  no_cert_pages <- length(list.files(path = cert_dir, pattern = "^cert_.*\\.png$", full.names = TRUE))
 
+  # Creating a list of images to slide through based on number of cert pages
+  list_images <- paste0('"cert_', 1:no_cert_pages, '.png"', collapse = ", ")
+  # Replacing the list of images for the slider
+  md_content <- gsub("\\$var_images\\$", 
+                     paste0("var images = [", list_images, "];"), 
+                     md_content)
+  
   # Adding the abstract
-  abstract <- get_abstract(register[i, ]$Repo)
+  abstract <- get_abstract(repo_link)
   md_content <- gsub("\\$abstract\\$", abstract, md_content)
 
   # Saving the md file
-  temp_dir <- paste0(CONFIG$CERTS_DIR[["cert"]], cert_id, "temp.md")
-  writeLines(md_table, temp_dir)
+  md_file_path <- paste0(cert_dir, "temp.md")
+  writeLines(md_content, md_file_path)
 }
 
 render_cert_html <- function(cert_id){
-  create_cert_md(cert_id)
 
-  output_dir <- paste0(CONFIG$CERTS_DIR[["cert"]], cert_id)
+  output_dir <- paste0(CONFIG$CERTS_DIR[["cert"]], cert_id, "/")
   temp_md_path <- paste0(output_dir, "temp.md")
 
   # Creating html document yml
+  create_cert_page_section_files(output_dir)
   generate_html_document_yml(output_dir)
-  yaml_path <- normalizePath(file.path(getwd(), paste0(output_dir, "html_document.yml")))
 
+  yaml_path <- normalizePath(file.path(getwd(), paste0(output_dir, "html_document.yml")))
+  
   # Render HTML from markdown
   rmarkdown::render(
     input = temp_md_path,
@@ -106,6 +121,14 @@ render_cert_html <- function(cert_id){
 create_cert_page_section_files <- function(output_dir){
 
   # Create prefix 
-  prefix_template <- readLines(CONFIG$TEMPLATE_DIR[["reg"]][["prefix"]], warn = FALSE)
+  prefix_template <- readLines(CONFIG$TEMPLATE_DIR[["cert"]][["prefix"]], warn = FALSE)
   writeLines(prefix_template, paste0(output_dir, "index_prefix.html"))
+
+  # Create postfix
+  postfix_template <- readLines(CONFIG$TEMPLATE_DIR[["cert"]][["postfix"]], warn = FALSE)
+  writeLines(postfix_template, paste0(output_dir, "index_postfix.html"))
+
+  # Create header
+  header_template <- readLines(CONFIG$TEMPLATE_DIR[["cert"]][["header"]], warn = FALSE)
+  writeLines(header_template, paste0(output_dir, "index_header.html"))
 }
--- R/download_certs.R
+++ R/download_certs.R
@@ -2,28 +2,46 @@ library(httr)
 library(jsonlite)
 
 download_cert_pdf <- function(report_link, cert_id){
-  # Obtaining the pdf download link from the report link
-  cert_download_url <- get_cert_link(report_link)
-
   # Checking if the certs dir exist
-  cert_pdf_dir <- CONFIG$CERTS_DIR[["certs_pdf"]]
-  pdf_path <- paste0(cert_id, "cert.pdf") 
+  cert_dir <- CONFIG$CERTS_DIR[["cert"]]
+  cert_sub_dir <- paste0(cert_dir, cert_id, "/")
+
+  if (!dir.exists(cert_sub_dir)) {
+    dir.create(cert_sub_dir, recursive = TRUE)
+  }
 
   # Download the PDF file
-  download_response <- GET(cert_download_url, write_disk(pdf_path, overwrite = TRUE))
+  pdf_path <- paste0(cert_sub_dir, "/cert.pdf") 
 
-  # Throwing warning messages
-  if (status_code(download_response) == 200) {
-      message(paste("Cert", cert_id, "downloaded successfully"))
-  } 
+  # Obtaining the pdf download link from the report link
+  cert_download_url <- get_cert_link(report_link, cert_id)
 
+  # Could not find a cert download url
+  if (is.na(cert_download_url)){
+    warning(paste("Failed to download the file. No link will be added to the cert", cert_id, ". No link will 
+    be added to this cert."))
+    return(0)
+  }
+
+  # Found a cert download url
   else{
-      warning(paste("Failed to download the file. No link will be added to the cert", cert_id, ". No link will 
+    download_response <- GET(cert_download_url, httr::write_disk(pdf_path, overwrite = TRUE))
+
+    if (status_code(download_response) == 200) {
+      message(paste("Cert", cert_id, "downloaded successfully"))
+      return(1)
+    } 
+
+    # Failed to download file
+    else{
+      warning(paste("Unsuccessful GET request to download certificate", cert_id, ". No link will 
       be added to this cert."))
+      return(0)
+    }
   }
 }
 
-get_cert_link <- function(report_link){
+get_cert_link <- function(report_link, cert_id){
   if (grepl("zenodo", report_link)){
     cert_download_url <- get_zenodo_cert_link(report_link, cert_id) 
   }
@@ -31,40 +49,69 @@ get_cert_link <- function(report_link){
   else if (grepl("OSF", report_link, ignore.case = FALSE)) {
     cert_download_url <- get_osf_cert_link(report_link, cert_id)
   }
-
+  # print(report_link)
   return(cert_download_url)
 }
-
 get_osf_cert_link <- function(report_link, cert_id){
-  # Retrieve the osf_project_id 
+  # Retrieve the OSF project node ID 
   node_id <- basename(report_link)
 
-  # Prepare the API endpoint to access files for a specific node and make the request
+  # Prepare the API endpoint to access files for a specific node
   files_url <- paste0(CONFIG$CERT_LINKS[["osf_api"]], "nodes/", node_id, "/files/osfstorage/")
-  response <- GET(files_url)
 
-  # Check if the request was successful
-  if (status_code(response) == 200) {
-      # Manually parse the JSON content
-      file_data <- fromJSON(content(response, "text", encoding = "UTF-8"))    
-      
-      # Accessing the files and finding the correct pdf file based on the extension and the file name
-      files_list <- file_data$data
+  # Initializing a list of all files (to handle pagination)
+  all_files <- list()
 
-      target_files <- files_list[grepl("\\.pdf$", files_list$attributes$name, ignore.case = TRUE), ]
-      
-      if (length(nrow(target_files)) > 1){
-          warning(paste("Multiple pdf files found in the OSF url. Cannot determine the correct OSF cert file to download. No link will be added to cert", cert_id))
-      }
-
-      target_file <- target_files[1, ]
-      return (target_file$links$download)
+  # Continue making requests while there is a 'next' page
+  while (!is.null(files_url)) {
+    response <- GET(files_url)
+    
+    # Check if the request was successful
+    if (status_code(response) != 200) {
+      stop("Failed to retrieve files: ", status_code(response))
+    }
+    
+    # Parse the response content
+    response_content <- content(response, as = "parsed", type = "application/json")
+    
+    # Add the files from the current page to the list of all files
+    all_files <- c(all_files, response_content$data)
+    
+    # Check if there is a 'next' link to retrieve the next page of files
+    files_url <- response_content$links[["next"]]
   }
 
-  else {
-      warning(paste("Could not access the OSF API. Skipping retrieving cert", cert_id))
+  # If no files were retrieved, warn and return NA
+  if (length(all_files) == 0) {
+    warning(paste("No files found for node", node_id))
+    return(NA)
   }
+
+  # Filter the files to find PDF files based on their extension in the name attribute
+  pdf_files <- lapply(all_files, function(file) {
+    if (grepl("\\.pdf$", file$attributes$name, ignore.case = TRUE)) {
+      return(file)
+    }
+    return(NULL)
+  })
+
+  # Remove NULL entries
+  pdf_files <- Filter(Negate(is.null), pdf_files)
+
+  # If multiple or no PDF files are found, return a warning
+  if (length(pdf_files) > 1) {
+    warning(paste("Multiple PDF files found in the OSF node. Cannot determine the correct cert file to download for cert", cert_id))
+    return(NA)
+  } else if (length(pdf_files) == 0) {
+    warning(paste("No PDF certs found for certificate with id", cert_id))
+    return(NA)
+  }
+
+  # Extract the download link for the target PDF file
+  cert_file <- pdf_files[[1]]
+  return(cert_file$links$download)
 }
+
 
 get_zenodo_cert_link <- function(report_link, cert_id, api_key = "") {
   # Set the base URL for the Zenodo API
@@ -72,25 +119,35 @@ get_zenodo_cert_link <- function(report_link, cert_id, api_key = "") {
   record_url <- paste0(CONFIG$CERT_LINKS[["zenodo_api"]], record_id, "/files")
   
   # Make the API request
-  response <- GET(record_url, add_headers(Authorization = paste("Bearer", api_key)))
+  response <- GET(record_url, httr::add_headers(Authorization = paste("Bearer", api_key)))
   
   # Check if the request was successful
   if (status_code(response) == 200) {
-   # Parse the response
+    
+    # Parse the response
     record_data <- fromJSON(content(response, "text", encoding = "UTF-8"))
     
     files_list <- record_data$entries
+    # print("List of files")
+    # print(files_list)
 
+    # print(nrow(files_list))
+    # print(files_list$key)
     # Check for files in the record
     if (!is.null(files_list)) {
-      target_files <- files_list[grepl("\\.pdf$", files_list$key, ignore.case = TRUE), ]
-      
-      if (length(nrow(target_files)) > 1){
-        warning(paste("Multiple pdf files found in the Zenodo url. Cannot determine the correct Zenodo cert file to download. No link will be added to cert", cert_id))
+      pdf_files <- files_list[grepl("\\.pdf$", files_list$key, ignore.case = TRUE), ]
+      # print("The pdf files")
+      # print(pdf_files)
+      if (nrow(pdf_files) > 1) {
+        warning(paste("Multiple PDF files found in the Zenodo node. Cannot determine the correct cert file to download for cert", cert_id))
+        return(NA)
+      } else if (nrow(pdf_files) == 0) {
+        warning(paste("No PDF certs found for certificate with id", cert_id))
+        return(NA)
       }
 
-      target_file <- target_files[1, ]
-      return (target_file$links$content)
+      cert_file <- pdf_files[1, ]
+      return (cert_file$links$content)
     } 
   } 
   else {
@@ -99,7 +156,7 @@ get_zenodo_cert_link <- function(report_link, cert_id, api_key = "") {
 }
 
 get_abstract <- function(register_repo) {
-  config_yml <- get_codecheck_yml(register[i, ]$Repo)
+  config_yml <- get_codecheck_yml(register_repo)
 
   # Retrieving the paper DOI
   paper_link <- config_yml$paper$reference
